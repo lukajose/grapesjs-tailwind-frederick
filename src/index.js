f7de756c7656ce6457d92bf1aa56cb94a403762e
lukajose <lukajosegamulin@outlook.com>
2024-05-21 12:45:58 +1000

    adding new loader spinner

--- src/index.js
+++ src/index.js
@@ -34,6 +34,32 @@ export default (editor, opts = {}) => {
     ...options.i18n,
   });
 
+
+  const spinnerContainer = document.createElement('div');
+  spinnerContainer.id = 'spinner-container';
+  spinnerContainer.id = 'spinner-container';
+    spinnerContainer.style.position = 'fixed';
+    spinnerContainer.style.top = '50%';
+    spinnerContainer.style.left = '50%';
+    spinnerContainer.style.transform = 'translate(-50%, -50%)';
+    spinnerContainer.style.zIndex = '9999';
+    spinnerContainer.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="50" height="50">
+        <radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
+          <stop offset="0" stop-color="#F547E9"></stop>
+          <stop offset=".3" stop-color="#F547E9" stop-opacity=".9"></stop>
+          <stop offset=".6" stop-color="#F547E9" stop-opacity=".6"></stop>
+          <stop offset=".8" stop-color="#F547E9" stop-opacity=".3"></stop>
+          <stop offset="1" stop-color="#F547E9" stop-opacity="0"></stop>
+        </radialGradient>
+        <circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70">
+          <animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2s" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform>
+        </circle>
+        <circle transform-origin="center" fill="none" opacity=".2" stroke="#F547E9" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle>
+      </svg>`;
+
+  document.body.appendChild(spinnerContainer);
+
   const appendTailwindCss = async (frame) => {
     const iframe = frame?.view?.getEl();
 
@@ -51,20 +77,24 @@ export default (editor, opts = {}) => {
 
     const cssStyle = document.createElement('style');
     cssStyle.innerHTML = cover;
-    
-
+    iframe.style.visibility = 'hidden';
     // checks iframe is ready before loading Tailwind CSS - issue with firefox
     const f = setInterval(() => {
       const doc = iframe.contentDocument;
       if (doc && doc.readyState && doc.readyState === 'complete') {
         doc.head.appendChild(script);
         doc.head.appendChild(cssStyle);
+        setTimeout(() => {
+          document.body.removeChild(spinnerContainer);
+          iframe.style.visibility = 'visible';
+        }, 100);
         clearInterval(f);
       }
-    }, 100)
+    }, 100);
   }
 
   editor.Canvas.getModel()['on']('change:frames', (m, frames) => {
+    
     frames.forEach((frame) => frame.once('loaded', () => appendTailwindCss(frame)));
   });
 
