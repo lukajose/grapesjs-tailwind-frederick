171509722fad2eb8e000c89b93149a7577cd5afe
lukajose <lukajosegamulin@outlook.com>
2024-07-23 18:50:38 +1000

    fixing bug when new frame changes the stale config does not change

--- src/index.js
+++ src/index.js
@@ -9,10 +9,52 @@ import themes from './themes';
 
 
 export default (editor, opts = {}) => {
-  const options = {
+  // const options = {
+  //   ...{
+  //     i18n: {},
+  //     // default options
+  //     tailwindPlayCdn: 'https://cdn.tailwindcss.com',
+  //     plugins: [], 
+  //     config: {
+  //       darkMode: ['selector', '[data-mode="dark"]'],
+  //       theme: {
+  //         screens: {
+  //           sm: '480px',
+  //           md: '768px',
+  //           lg: '976px',
+  //           xl: '1440px',
+  //         },
+  //         fontFamily: {
+  //           sans: ['Graphik', 'sans-serif'],
+  //           serif: ['Merriweather', 'serif'],
+  //         },
+  //         extend: {
+  //           spacing: {
+  //             '128': '32rem',
+  //             '144': '36rem',
+  //           },
+  //           colors: themes.light,
+  //           borderRadius: {
+  //             '4xl': '2rem',
+  //           }
+  //         }
+  //       }
+  //     },
+  //     cover: `.object-cover { filter: sepia(1) hue-rotate(190deg) opacity(.46) grayscale(.7) !important; } * {
+  //       cursor: url(https://e2bbdf25-7697-4747-b9cf-9badb086afbf.frederick-ai.com/assets/editor-cursor.png), default;
+  //       *:hover {
+  //         cursor: url(https://e2bbdf25-7697-4747-b9cf-9badb086afbf.frederick-ai.com/assets/editor-cursor.png), pointer;
+  //       }
+  //     }
+  //     `,
+  //     changeThemeText: 'Change Theme',
+  //     openCategory: 'Blog',
+  //   }, ...opts
+  // };
+
+  const getLatestOptions = () => ({
     ...{
       i18n: {},
-      // default options
       tailwindPlayCdn: 'https://cdn.tailwindcss.com',
       plugins: [],
       config: {
@@ -49,8 +91,12 @@ export default (editor, opts = {}) => {
       `,
       changeThemeText: 'Change Theme',
       openCategory: 'Blog',
-    }, ...opts
-  };
+    },
+    ...opts
+  });
+
+  // Initial options
+  let options = getLatestOptions();
 
   // Add blocks
   loadBlocks(editor, options);
@@ -63,28 +109,28 @@ export default (editor, opts = {}) => {
   });
 
 
-  const spinnerContainer = document.createElement('div');
-  spinnerContainer.id = 'spinner-container';
-  spinnerContainer.id = 'spinner-container';
-    spinnerContainer.style.position = 'fixed';
-    spinnerContainer.style.top = '50%';
-    spinnerContainer.style.left = '50%';
-    spinnerContainer.style.transform = 'translate(-50%, -50%)';
-    spinnerContainer.style.zIndex = '9999';
-    spinnerContainer.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="50" height="50">
-        <radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
-          <stop offset="0" stop-color="#F547E9"></stop>
-          <stop offset=".3" stop-color="#F547E9" stop-opacity=".9"></stop>
-          <stop offset=".6" stop-color="#F547E9" stop-opacity=".6"></stop>
-          <stop offset=".8" stop-color="#F547E9" stop-opacity=".3"></stop>
-          <stop offset="1" stop-color="#F547E9" stop-opacity="0"></stop>
-        </radialGradient>
-        <circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70">
-          <animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2s" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform>
-        </circle>
-        <circle transform-origin="center" fill="none" opacity=".2" stroke="#F547E9" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle>
-      </svg>`;
+  // const spinnerContainer = document.createElement('div');
+  // spinnerContainer.id = 'spinner-container';
+  // spinnerContainer.id = 'spinner-container';
+  //   spinnerContainer.style.position = 'fixed';
+  //   spinnerContainer.style.top = '50%';
+  //   spinnerContainer.style.left = '50%';
+  //   spinnerContainer.style.transform = 'translate(-50%, -50%)';
+  //   spinnerContainer.style.zIndex = '9999';
+  //   spinnerContainer.innerHTML = `
+  //     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="50" height="50">
+  //       <radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
+  //         <stop offset="0" stop-color="#F547E9"></stop>
+  //         <stop offset=".3" stop-color="#F547E9" stop-opacity=".9"></stop>
+  //         <stop offset=".6" stop-color="#F547E9" stop-opacity=".6"></stop>
+  //         <stop offset=".8" stop-color="#F547E9" stop-opacity=".3"></stop>
+  //         <stop offset="1" stop-color="#F547E9" stop-opacity="0"></stop>
+  //       </radialGradient>
+  //       <circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70">
+  //         <animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2s" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform>
+  //       </circle>
+  //       <circle transform-origin="center" fill="none" opacity=".2" stroke="#F547E9" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle>
+  //     </svg>`;
 
   // document.body.appendChild(spinnerContainer);
   const appendTailwindCss = async (frame) => {
@@ -92,7 +138,8 @@ export default (editor, opts = {}) => {
 
     if (!iframe) return;
 
-    const { tailwindPlayCdn, plugins, config, cover } = options;
+    const latestOptions = getLatestOptions();
+    const { tailwindPlayCdn, plugins, config, cover } = latestOptions;
     const init = () => {
       // @ts-ignore
       iframe.contentWindow.tailwind.config = config;
